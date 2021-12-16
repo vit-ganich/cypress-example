0054f23fe924d4b4e5d5cea95436356332267bd4
vhanich-es <vitali.hanich@effective-soft.com>
2021-12-16 19:38:12 +0100

    Updated ci

--- cypress/integration/todo.spec.ts
+++ cypress/integration/todo.spec.ts
@@ -9,6 +9,8 @@ describe('example to-do app', () => {
 
   it('shows environment variables', () => {
     // Examle of accessing the environment variables from Cypress.env
+    cy.log(JSON.stringify(Cypress.env));
+    console.log(Cypress.env);
     cy.wrap(Cypress.env('USER_NAME')).should('equal', 'test');
 
     const userName = process.env.USER_NAME || 'empty';
